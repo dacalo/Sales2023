fe4dc3d80cfda3d864f389ba28e8d77afa53bddc
David Chávez López <dacalo.soporte@gmail.com>
2023-03-15 10:26:18 -0600

    Tutorial 69 - Parte 16 - Adicionar foto al usuario

--- Sales.API/Helpers/FileStorage.cs
+++ Sales.API/Helpers/FileStorage.cs
@@ -5,15 +5,15 @@ namespace Sales.API.Helpers
 {
     public class FileStorage : IFileStorage
     {
-        private readonly string connectionString;
+        private readonly string _connectionString;
         public FileStorage(IConfiguration configuration)
         {
-            connectionString = configuration.GetConnectionString("AzureStorage")!;
+            _connectionString = configuration.GetConnectionString("AzureStorage")!;
         }
 
         public async Task RemoveFileAsync(string path, string containerName)
         {
-            var client = new BlobContainerClient(connectionString, containerName);
+            var client = new BlobContainerClient(_connectionString, containerName);
             await client.CreateIfNotExistsAsync();
             var fileName = Path.GetFileName(path);
             var blob = client.GetBlobClient(fileName);
@@ -22,7 +22,7 @@ namespace Sales.API.Helpers
 
         public async Task<string> SaveFileAsync(byte[] content, string extention, string containerName)
         {
-            var client = new BlobContainerClient(connectionString, containerName);
+            var client = new BlobContainerClient(_connectionString, containerName);
             await client.CreateIfNotExistsAsync();
             client.SetAccessPolicy(PublicAccessType.Blob);
             var fileName = $"{Guid.NewGuid()}{extention}";
